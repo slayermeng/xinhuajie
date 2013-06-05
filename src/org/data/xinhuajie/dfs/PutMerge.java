a25c81cec126d3424b2d5b38426c133ba5403d3f
slayermeng <mengxin83@gmail.com>
2013-06-05 11:19:32 +0800

    增加putmerger程序

---
+++ src/org/data/xinhuajie/dfs/PutMerge.java
@@ -0,0 +1,12 @@
+package org.data.xinhuajie.dfs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+public class PutMerge {
+	public static void main(String[] args) throws IOException{
+		Configuration conf = new Configuration();
+		
+	}
+}
